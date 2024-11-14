430b1a1ac4f1739d698070e650e7776518b46710
hsin yu <tina2793778@gmail.com>
2024-11-14 13:24:18 +0800

    feat: 🎸 設定 nuxt config 全域 scss 變數

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '@/assets/stylesheets/all.scss', 
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "bootstrap/scss/functions";
+            @import "@/assets/stylesheets/_variables.scss";
+            @import "bootstrap/scss/variables";
+            @import "bootstrap/scss/variables-dark";
+          `,
+        },
+      },
+    },
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,21 @@
 
 <template>
   <div>
-    Page: 首頁
+    <h1>Page: 首頁</h1>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-success">Success</button>
+    <button class="btn btn-danger">Danger</button>
+    <button class="btn btn-warning">Warning</button>
+    <button class="btn btn-info">Info</button>
+    <button class="btn btn-light">Light</button>
+    <button class="btn btn-dark">Dark</button>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+h1 {
+  color: $primary;
+  line-height: $line-height-lg;
+}
+</style>

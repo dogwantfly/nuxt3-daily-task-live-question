b01ade450b4151f96ae09d578f90748fef5e6f02
hsin yu <tina2793778@gmail.com>
2024-11-14 23:00:19 +0800

    feat: 🎸 新增 room、room/_id 嵌套路由

---
+++ pages/room/_id.vue
@@ -0,0 +1,41 @@
+<script setup>
+const route = useRoute();
+const router = useRouter();
+const goBack = () => {
+  router.go(-1);
+};
+</script>
+
+<template>
+  <h1>房型詳細頁面</h1>
+  <h2>{{ route.fullPath }}</h2>
+  <button class="btn" @click="goBack">回上一頁</button>
+</template>
+
+<style scoped>
+h1 {
+  margin-bottom: 20px;
+}
+
+.btn {
+  background-color: rgba(51, 51, 51, 0.05);
+  border-radius: 8px;
+  border-width: 0;
+  color: #333333;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  list-style: none;
+  margin: 0;
+  padding: 10px 12px;
+  text-align: center;
+  transition: all 200ms;
+  vertical-align: baseline;
+  white-space: nowrap;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+</style>
--- pages/room/index.vue
+++ pages/room/index.vue
@@ -1,17 +1,32 @@
 <script setup>
+const router = useRouter();
 const roomsList = ref([]);
 // 使用 fetch 或 axios 串接 前台房型 API ( GET )
 // apiUrl : https://nuxr3.zeabur.app/api/v1/rooms
 // response 回傳後，將資料寫入 roomsList 變數
 // 使用 roomsList 變數在下方 template 渲染列表
+try {
+  const response = await fetch('https://nuxr3.zeabur.app/api/v1/rooms');
+  if (!response.ok) {
+    throw new Error('取得房型資料失敗');
+  }
+  const data = await response.json();
+  roomsList.value = data.result;
+} catch (error) {
+  console.error(error);
+}
 </script>
 
 <template>
   <h1>房型頁面</h1>
-  <!-- <div class="container mt-4">
+  <div class="container mt-4">
     <div class="row justify-content-center">
-      <div class="col-8 col-md-6 col-lg-3" v-for="...">
-        <div class="card h-100 shadow-sm">
+      <div
+        class="col-8 col-md-6 col-lg-3"
+        v-for="room in roomsList"
+        :key="room._id"
+      >
+        <div @click="router.push(`/room/_id`)" class="card h-100 shadow-sm">
           <img :src="room.imageUrl" class="card-img-top" alt="Room Image" />
           <div class="card-body d-flex flex-column">
             <h3 class="card-title">{{ room.name }}</h3>
@@ -26,7 +41,7 @@ const roomsList = ref([]);
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
 </template>
 
 <style scoped>

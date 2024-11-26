e58c1f45fe35076507b36c532cc45acdbaf8f5a1
hsin yu <tina2793778@gmail.com>
2024-11-26 14:06:50 +0800

    feat: 🎸 新增登入功能

--- pages/login.vue
+++ pages/login.vue
@@ -7,7 +7,7 @@ const userLoginObject = ref({
   password: "",
 });
 
-const loginAccount = () => {
+const loginAccount = async () => {
   /*
   1. 串接旅館的 登入 API
   2. 登入成功後，使用 useCookie() 將 token 寫入名稱為 “auth” 的 cookie
@@ -21,6 +21,32 @@ const loginAccount = () => {
    timer: 1500,
  });
   */
+  try {
+    const { data } = await useFetch(
+      "https://nuxr3.zeabur.app/api/v1/user/login",
+      {
+        method: "POST",
+        body: userLoginObject.value,
+      }
+    );
+    const token = data.value.token;
+    useCookie("auth").value = token;
+    $swal.fire({
+      position: "center",
+      icon: "success",
+      title: "登入成功",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } catch (error) {
+    $swal.fire({
+      position: "center",
+      icon: "error",
+      title: "登入失敗",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
 };
 </script>
 
@@ -30,7 +56,7 @@ const loginAccount = () => {
       <div class="row justify-content-md-center">
         <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
           <h2 class="h3 mb-4">登入</h2>
-          <form>
+          <form @submit.prevent="loginAccount">
             <div class="form-floating mb-4">
               <input
                 type="email"
@@ -39,6 +65,7 @@ const loginAccount = () => {
                 placeholder="example@gmail.com"
                 pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
                 required
+                v-model="userLoginObject.email"
               />
               <label for="email">信箱 <span class="text-danger">*</span></label>
             </div>
@@ -51,6 +78,7 @@ const loginAccount = () => {
                 placeholder="請輸入 8 碼以上密碼"
                 pattern=".{8,}"
                 required
+                v-model="userLoginObject.password"
               />
               <label for="password"
                 >密碼 <span class="text-danger">*</span></label
